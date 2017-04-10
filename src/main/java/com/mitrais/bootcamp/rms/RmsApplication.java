7e37adfbef49bbe969ac7f189d4bb69b7e10fec0
angelairfani <irfani.angela@gmail.com>
2017-04-10 07:37:26 +0700

    #38 remove init data part

--- src/main/java/com/mitrais/bootcamp/rms/RmsApplication.java
+++ src/main/java/com/mitrais/bootcamp/rms/RmsApplication.java
@@ -1,38 +1,13 @@
 package com.mitrais.bootcamp.rms;
 
-import com.mitrais.bootcamp.rms.service.EmployeeService;
-import com.mitrais.bootcamp.rms.service.JobFamilyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RmsApplication implements CommandLineRunner {
-
-	@Autowired
-	private JobFamilyService jobFamilyService;
-
-	@Autowired
-	private EmployeeService employeeService;
+public class RmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RmsApplication.class, args);
 	}
 
-
-	/**
-	 * Method invoked after this class has been instantiated by Spring container
-	 * Initializes the in-memory database with all the TourPackages and Tours.
-	 *
-	 * @param strings
-	 * @throws Exception if problem occurs.
-	 */
-	@Override
-	public void run(String... strings) throws Exception {
-
-//		jobFamilyService.initJobFamily();
-//		employeeService.employeeExampleData();
-
-	}
 }
