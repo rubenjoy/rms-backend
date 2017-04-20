9b354ec9889fe8107822e9d50c129122f4b50641
rubenjoy <rubenjoy@gmail.com>
2017-04-20 16:35:47 +0800

    add test for employee getcontroller and postcontroller

---
+++ src/test/java/com/mitrais/rms/controller/employee/GetControllerTest.java
@@ -0,0 +1,93 @@
+package com.mitrais.rms.controller.employee;
+
+import com.mitrais.rms.controller.employee.GetController;
+import com.mitrais.rms.entity.Employee;
+import com.mitrais.rms.entity.enumerated.Gender;
+import com.mitrais.rms.entity.enumerated.MaritalStatus;
+import com.mitrais.rms.entity.enumerated.Nationality;
+import static com.mitrais.rms.utils.DateFormatter.parse;
+
+import io.restassured.http.ContentType;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@DataJpaTest
+@ComponentScan
+@RunWith(SpringRunner.class)
+public class GetControllerTest
+{
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Autowired
+	@Qualifier("employeeGetController")
+	private GetController getController;
+
+	private Integer createdId;
+
+	@Before
+	public void setup()
+	{
+		Employee e = new Employee();
+		e.setName("Mukidi")
+			.setBirthDate(parse("2017-12-12"))
+			.setGender(Gender.MALE)
+			.setMaritalStatus(MaritalStatus.SINGLE)
+			.setPhone("0812")
+			.setEmail("mukidi@gmail.com")
+			.setHireDate(parse("2016-12-12"))
+			.setActiveInd(true)
+			.setEmploymentStatus("contract")
+			.setSuspendDate(parse("2016-12-12"))
+			.setJobFamily("SE")
+			.setJobTitle("PG")
+			.setDivision("CDC Java")
+			.setBusinessUnit("buscom")
+			.setNationality(Nationality.INDONESIAN);
+		entityManager.persist(e);
+		createdId = e.getId();
+		assert createdId != null;
+	}
+
+	@Test
+	public void itShouldFindById()
+	{
+		given().
+			standaloneSetup(this.getController).
+		when().
+			get("/employees/{employeeId}", createdId.intValue()).
+		then().
+			statusCode(200).
+			contentType(ContentType.JSON).
+			body("id", equalTo("/employees/" + createdId.toString())).
+			body("name", equalTo("Mukidi")).
+			body("birthDate", equalTo("2017-12-12"));
+	}
+
+	@Test
+	public void itShouldFindAll()
+	{
+		given().
+			standaloneSetup(this.getController).
+		when().
+			get("/employees").
+		then().
+			statusCode(200).
+			contentType(ContentType.JSON).
+			body("id", hasItems("/employees/" + createdId.toString())).
+			body("name", hasItems("Mukidi"));
+	}
+}
---
+++ src/test/java/com/mitrais/rms/controller/employee/PostControllerTest.java
@@ -0,0 +1,87 @@
+package com.mitrais.rms.controller.employee;
+
+import java.lang.reflect.Field;
+
+import com.mitrais.rms.entity.Employee;
+import com.mitrais.rms.repository.EmployeeRepository;
+
+import io.restassured.http.ContentType;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.mockito.ArgumentMatchers.any;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class PostControllerTest
+{
+	@MockBean
+	private EmployeeRepository employee;
+
+	@Autowired
+	private PostController postController;
+
+	@Configuration
+	@Import(PostController.class)
+	static class Config {}
+
+	@Before
+	public void setup() 
+	{
+		Mockito.when(employee.save(any(Employee.class))).
+			thenAnswer(
+				new Answer() {
+					public Object answer(InvocationOnMock invocation) {
+						Object[] args = invocation.getArguments();
+						if (args.length > 0) {
+							Object obj = args[0];
+							if (obj instanceof Employee) {
+								try {
+									Field id = obj.getClass().getDeclaredField("id");
+									id.setAccessible(true);
+									id.set(obj, new Integer(2));
+									return obj;
+								} catch(Exception e) {
+									System.err.println(e.getMessage());
+								}
+							}
+							return obj;
+						}
+						return null;
+					}
+				}
+			);
+	}
+
+	@Test
+	public void itShouldReturnCreatedUser()
+	{
+		String employee = "{\"name\": \"Mukidi\", \"birthDate\": \"2017-12-12\", \"hireDate\": \"2017-12-12\", \"suspendDate\": \"2017-12-12\"}";
+
+		given().
+			contentType("application/json").
+			body(employee).
+			standaloneSetup(this.postController).
+		when().
+			post("/employees").
+		then().
+			statusCode(200).
+			contentType(ContentType.JSON).
+			body("id", equalTo("/employees/2")).
+			body("name", equalTo("Mukidi")).
+			body("birthDate", equalTo("2017-12-12"));
+	}
+}
